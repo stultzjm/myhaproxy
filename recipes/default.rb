2c022cea466a8e2799ee240094fee4429e8aebdb
stultzjm <joshuastultz@yahoo.com>
2018-01-10 11:19:02 -0500

    initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-54-144-41-15.compute-1.amazonaws.com',
+    'ipaddress' => '54.144.41.15',
+    'port' => 80,
+    'ssl_port' => 80
+  }]
+
+include_recipe 'haproxy::manual'
